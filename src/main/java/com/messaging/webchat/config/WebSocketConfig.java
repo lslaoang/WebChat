85cd87551dca6c975887acb9de0b0702c503ab21
Laurel Lao Ang <laurellaoang@gmail.com>
2021-10-22 18:19:44 +0800

    ChatMessage added as model

--- src/main/java/com/messaging/webchat/config/WebSocketConfig.java
+++ src/main/java/com/messaging/webchat/config/WebSocketConfig.java
@@ -1,4 +1,4 @@
-package com.messaging.webchat;
+package com.messaging.webchat.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
